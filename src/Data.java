be17262df051b84a86a0b1f459a5f2b245cf6be7
Grupa 2 Java <ciocos_lm@yahoo.com>
2018-11-09 18:00:31 +0200

    import

---
+++ src/Data.java
@@ -0,0 +1,15 @@
+public class Data {
+    public static void main(String[] args) {
+        int myAge = 32;
+        float weight = 99.7f;
+        double height = 209.4;
+        long distance = 43242342342423l;
+        char gender = 'm';
+        char test = 400;
+        boolean isStudent = true;
+        boolean hasJob = false;
+        int test2 = (int) distance;
+        System.out.print(test);
+        System.out.print(test2);
+    }
+}
